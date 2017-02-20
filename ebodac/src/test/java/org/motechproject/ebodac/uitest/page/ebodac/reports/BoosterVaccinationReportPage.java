8eb9b018a5ce1b6e24b2490b0aad7989d50de2d1
Pawel Muchowski <pmuchowski@soldevelo.com>
2017-02-20 17:21:04 +0100

    EBODAC-804: Booster Vaccination Report ui-test

--- ebodac/src/test/java/org/motechproject/ebodac/uitest/page/ebodac/reports/BoosterVaccinationReportPage.java
+++ ebodac/src/test/java/org/motechproject/ebodac/uitest/page/ebodac/reports/BoosterVaccinationReportPage.java
@@ -1,11 +1,14 @@
 package org.motechproject.ebodac.uitest.page.ebodac.reports;
 
 import org.motechproject.ebodac.uitest.page.ebodac.EbodacPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class BoosterVaccinationReportPage extends EbodacPage {
 
     public static final String URL_PATH = "/home#/ebodac/reportBoosterVaccination";
+
+    private static final By NUMBER_OF_ROWS = By.xpath("//*[@id='pageInstancesTable_left']/div");
 
     public BoosterVaccinationReportPage(WebDriver driver) {
         super(driver);
@@ -14,5 +17,9 @@ public class BoosterVaccinationReportPage extends EbodacPage {
     @Override
     public String expectedUrlPath() {
         return URL_PATH;
+    }
+
+    public boolean isReportEmpty() {
+        return findElement(NUMBER_OF_ROWS).getAttribute("innerHTML").contains("No records to view");
     }
 }
